a361e2966d58edad21ae0ff3522a319cdb2996ec
deadprogram <ron@hybridgroup.com>
2013-09-28 18:09:42 -0700

    Supposed to be working BlinkM commands

--- lib/artoo/drivers/blink_m.rb
+++ lib/artoo/drivers/blink_m.rb
@@ -4,7 +4,7 @@ module Artoo
   module Drivers
     # BlinkM LED driver behaviors for i2c
     class BlinkM < Driver
-      COMMANDS = [:rgb, :fade].freeze
+      COMMANDS = [:rgb, :fade, :color, :firmware_version].freeze
 
       def address; 0x09; end
 
@@ -12,6 +12,7 @@ module Artoo
         begin
           connection.i2c_start(address)
           connection.i2c_write("o".bytes.first) # stops any scripts already running
+          rgb(0, 0, 0)
 
           super
         rescue Exception => e
@@ -22,13 +23,27 @@ module Artoo
       end
 
       def rgb(r=0, g=0, b=0)
-        connection.i2c_write("n".bytes.first)
+        connection.i2c_write('n'.bytes.first)
         connection.i2c_write(r, g, b)
       end
 
       def fade(r=0, g=0, b=0)
-        connection.i2c_write("c".bytes.first)
+        connection.i2c_write('c'.bytes.first)
         connection.i2c_write(r, g, b)
+      end
+
+      def firmware_version
+        connection.i2c_write('Z'.bytes.first)
+        @data = connection.i2c_read(2)
+        return nil if @data.nil? || @data.empty?
+        return "#{@data[0]}.#{@data[1]}"
+      end
+
+      def color
+        connection.i2c_write('g'.bytes.first)
+        @data = connection.i2c_read(3)
+        return nil if @data.nil? || @data.empty?
+        return @data[0], @data[1], @data[2]
       end
     end
   end
